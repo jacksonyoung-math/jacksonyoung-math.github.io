f093c83c140a038a07a6e236db8e0c09cdc4ddbd
united9jackson <74790194+united9jackson@users.noreply.github.com>
2026-09-04 11:41:40 -0700

    Update app.js

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
-$(document).ready(function(){
+t$(document).ready(function(){
 
 	/* redraws a string, character by character, given a div tag with 
 	title="string" and a list of letter divs contained in the word. */
 	$.fn.redrawHeader = function() {
 		var namelist = this.attr("title").split("");
 		jQuery.each(namelist, function(i) {
-			$("#name"+i).hide();
+			$("#name"+i).fadeOut(0);
 		});
 		jQuery.each(namelist, function(i) {
 			$("#name"+i).delay(100*3*i).fadeIn();
